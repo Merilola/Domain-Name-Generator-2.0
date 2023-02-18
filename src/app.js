d081bcbe1606bc9556fba1622a8440577144c567
Merilola <mariahef12@hotmail.com>
2023-02-18 00:11:13 +0000

    domainGenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoum = ["the", "our"];
+  const adjt = ["great", "big"];
+  const subjet = ["jogger", "racoon"];
+  const extension = [".com", ".es", ".it"];
+  getDomain(pronoum, adjt, subjet, extension);
+};
+const getDomain = (pronoum, adjt, subjet, extension) => {
+  for (let i = 0; i < pronoum.length; i++) {
+    for (let a = 0; a < adjt.length; a++) {
+      for (let e = 0; e < subjet.length; e++) {
+        for (let u = 0; u < extension.length; u++) {
+          console.log(`${pronoum[i]} ${adjt[a]} ${subjet[e]} ${extension[u]}`);
+        }
+      }
+    }
+  }
 };
